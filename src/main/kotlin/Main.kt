d9c0f247a60f33e1fa0e5095e5eb31af4c9d8d46
Ricky Ultimate <rickyogigi@gmail.com>
2023-11-16 10:44:42 +0100

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 fun main(){
     val coffeeMachine = PremiumCoffeeMachine(23.5,"gold")
+    val coffeeMachine2 = PremiumCoffeeMachine()
+
     coffeeMachine.makeCoffee2()
     coffeeMachine.makeCoffee()
+
+    println("")
+
+    coffeeMachine2.makeCoffee2()
 }
--- src/main/kotlin/PremiumCoffeeMachine.kt
+++ src/main/kotlin/PremiumCoffeeMachine.kt
@@ -4,7 +4,7 @@ class PremiumCoffeeMachine(
     private val quantity: Double = 2.0
 ): BaseCoffeeMachine(9999.9,"random") {
 
-    //constructor(): this(0.0, "brown", 0.0)
+    constructor(): this(0.0, "brown", 0.0)
 
     fun makeCoffee2(){
         println("Your $color coloured coffee of price $${price} and quantity ${quantity}cm^3 is ready")
